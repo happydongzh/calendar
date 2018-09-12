d91592efa6fb11dd5194571daed96da713a831fd
happydongzh <lydong@cn.ibm.com>
2018-09-12 15:29:23 +0800

    update

--- js/calendar.js
+++ js/calendar.js
@@ -106,10 +106,98 @@ $(function($) {
 
     };
 
+
+    function updateYearCalendar(direction = '') {
+        function getDistance(dir) {
+            let dis = 0;
+            switch (dir) {
+                case 'prev':
+                    dis = '10%';
+                    break;
+                case 'next':
+                    dis = '-10%';
+                    break;
+
+                default:
+                    dis = 0;
+                    break;
+            }
+            return dis;
+        };
+        $('div.abc').children().text(cal.getFullYear());
+
+        anime({
+            targets: calendar.children('div.month').children('div.days').toArray(),
+            translateX: getDistance(direction),
+            opacity: 0,
+            easing: 'easeInQuint',
+            easing: 'linear',
+            duration: 100,
+            complete: function(obj) {
+                obj.animatables.forEach((element, idx) => {
+                    $(element.target).css({
+                        transform: 'translateX(0px)'
+                    }).children().remove();
+                });
+                obj.animatables.forEach((element, idx) => {
+                    cal.setMonth((idx + 1), 0);
+                    monthCalendar(cal, $(element.target));
+                    anime({
+                        targets: element.target,
+                        opacity: 1,
+                        duration: 60,
+                        easing: 'easeInQuint'
+                    });
+                });
+            }
+        });
+    };
+
+    function mouseoverOnDay(e) {
+        dateDetail.css({
+            left: $(this).offset().left,
+            top: $(this).offset().top
+        });
+    }
+
+    function mouseClickOnDay(e) {
+        if (calendar.hasClass('blur')) {
+            return;
+        }
+        dateDetail.data('dd', $(this).attr('data-value'));
+        console.log($(this).attr('data-value'));
+        dateDetail.css({
+            left: $(this).offset().left,
+            top: $(this).offset().top
+        });
+        dateDetailOpened = !dateDetailOpened;
+        $(document.body).children().not('div.dateDetail').addClass('blur');
+        openDetail.call(dateDetail, $(this));
+    }
+
+
     //create year calendar dom
     yearCalendar(calendar, cal.getFullYear(), cal);
+    
     //create year headers dom
     $('<div class="yearTitle"><div class="yearDiv"><div class="abc">' + headerDivs(cal.getFullYear(), yearRange) + '</div></div></div>').insertBefore(calendar);
+
+    var colorStep = 255 / 6,
+        hl = dateDetail.find('div.hour').length;
+    dateDetail.find('div.hour').each(function(idx) {
+        let c = colorStep*(idx+1)/2;
+        if(idx >= 12){
+            c = colorStep*(hl - idx)/2;
+        }
+        $(this).html((idx >= 10)?(idx+':00'):('0'+idx+':00')).css({
+            'background-color': 'rgba('+Math.round(c)+','+Math.round(c)+','+Math.round(c)+',1)',
+            color:'rgba('+(255-Math.round(c))+','+(255-Math.round(c))+','+(255-Math.round(c))+',1)'
+        });//.append('<div class="close icon"></div>');
+        $(this).on('click', function(){
+            alert('Add event not finished yet, coming soon.');
+        });
+    });
+
 
     $('#prev').on('click', function() {
         if ($('div.yearDiv').data('expanded')) {
@@ -206,123 +294,9 @@ $(function($) {
     });
 
 
-    function updateYearCalendar(direction = '') {
-        function getDistance(dir) {
-            let dis = 0;
-            switch (dir) {
-                case 'prev':
-                    dis = '10%';
-                    break;
-                case 'next':
-                    dis = '-10%';
-                    break;
-
-                default:
-                    dis = 0;
-                    break;
-            }
-            return dis;
-        }
-        $('div.abc').children().text(cal.getFullYear());
-        anime({
-            targets: calendar.children('div.month').children('div.days').toArray(),
-            translateX: getDistance(direction),
-            opacity: 0,
-            easing: 'easeInQuint',
-            easing: 'linear',
-            duration: 100,
-            complete: function(obj) {
-                obj.animatables.forEach((element, idx) => {
-                    $(element.target).css({
-                        transform: 'translateX(0px)'
-                    }).children().remove();
-                });
-                obj.animatables.forEach((element, idx) => {
-                    cal.setMonth((idx + 1), 0);
-                    monthCalendar(cal, $(element.target));
-                    anime({
-                        targets: element.target,
-                        opacity: 1,
-                        duration: 60,
-                        easing: 'easeInQuint'
-                    });
-                });
-            }
-        });
-
-        /**
-        let yearTitle = anime({
-            targets: $('div.abc').get(0),
-            translateX: (yearWidth * count),
-            easing: [.91, -0.54, .29, 1.56],
-            duration: 200,
-            complete: function () {
-                anime({
-                    targets: calendar.children('div.month').toArray(),
-                    translateX: (yearWidth * count),
-                    rotateY: 45,
-                    scale: 1,
-                    opacity: 0.1,
-                    easing: [.91, -0.54, .29, 1.56],
-                    duration: 260,
-                    complete: function (obj) {
-                        //let month = new Date();
-                        //month.setFullYear(cal.getFullYear() - count);
-                        //console.log(month);
-                        console.log('===>' + cal);
-                        obj.animatables.forEach((element, idx) => {
-                            $(element.target).children('div.days').children().remove();
-                            cal.setMonth((idx + 1), 0);
-                            monthCalendar(cal, $(element.target).children('div.days'));
-                        });
-                        obj.reset();
-                    }
-                });
-            }
-        });
-         */
-    }
-
-    function mouseoverOnDay(e) {
-        dateDetail.css({
-            left: $(this).offset().left,
-            top: $(this).offset().top
-        });
-    }
-
-    function mouseClickOnDay(e) {
-        if (calendar.hasClass('blur')) {
-            return;
-        }
-        dateDetail.data('dd', $(this).attr('data-value'));
-        console.log($(this).attr('data-value'));
-        dateDetail.css({
-            left: $(this).offset().left,
-            top: $(this).offset().top
-        });
-        dateDetailOpened = !dateDetailOpened;
-        $(document.body).children().not('div.dateDetail').addClass('blur');
-        openDetail.call(dateDetail, $(this));
-    }
-
+   
     //calendar.children('div.month').on('mouseover', 'div.theDate', mouseoverOnDay);
     calendar.children('div.month').on('click', 'div.theDate', mouseClickOnDay);
-
-    var colorStep = 255 / 6,
-        hl = dateDetail.find('div.hour').length;
-    dateDetail.find('div.hour').each(function(idx) {
-        let c = colorStep*(idx+1)/2;
-        if(idx >= 12){
-            c = colorStep*(hl - idx)/2;
-        }
-        $(this).html((idx >= 10)?(idx+':00'):('0'+idx+':00')).css({
-            'background-color': 'rgba('+Math.round(c)+','+Math.round(c)+','+Math.round(c)+',1)',
-            color:'rgba('+(255-Math.round(c))+','+(255-Math.round(c))+','+(255-Math.round(c))+',1)'
-        });//.append('<div class="close icon"></div>');
-        $(this).on('click', function(){
-            alert('Add event not finished yet, coming soon.');
-        });
-    });
 
     $('#close').on('click', closeDetail);
 
@@ -418,38 +392,4 @@ $(function($) {
 
         
     };
-
-    // dateDetail.children('div.hours').on('mouseenter','div.hour', function(e){
-    //     $(this).parent().css({
-    //        backgroundColor: $(this).css('background-color') 
-    //     });
-    // });
-
-
-    // var clock = new Clock(document.getElementById('clock'), {
-    //     size: 450,
-    //     style:'night'
-    // });
-
-
-    // function highLightDate(target, clzz = 'highlight') {
-    //     if (!target) {
-    //         return;
-    //     }
-    //     //var dateStr = target.getFullYear() + '/' + (target.getMonth() + 1) + '/' + target.getDate();
-    //     var highLightDateStr = target.getFullYear() + '/' + (target.getMonth() + 1) + '/' + target.getDate();
-    //     //console.log(highLightDateStr);
-    //     var x = calendar.find('div.theDate[data-value="' + highLightDateStr + '"]');
-    //     x.children().addClass(clzz);
-    // }
-
-
-    //highLightDate(cal);
-
-    // var lydong = ['2018/2/15','2018/2/16','2018/2/17','2018/2/18','2018/2/19','2018/2/10','2018/2/11','2018/2/12',
-    // '2018/2/13','2018/2/14','2018/2/15','2018/2/16','2018/2/17','2018/2/18','2018/2/19','2018/2/20','2018/2/21','2018/2/24'];
-
-    // for (let d = 0; d < lydong.length; d++) {
-    //     highLightDate(lydong[d],"today");
-    // }
 });
